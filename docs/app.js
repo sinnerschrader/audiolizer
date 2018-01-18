78b4cc2ac963e617dd7af51a4fe5ca3e8a12b059
Gregor Adams <greg@pixelass.com>
2018-01-19 00:02:34 +0100

    chore(demo): new demo

--- docs/app.js
+++ docs/app.js
@@ -112,11 +112,10 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
   const W = 512;
   const H = 512;
   const LINE_WIDTH = 3;
-  const OPACITY = 1;
   const VOLUME = 0.2;
-
-  const ROUND = 1;
-  const part = 2 * Math.PI;
+  const OPACITY = 0.2;
+  const FF = 64;
+  const STEP = W / FF;
   const BG = '#000';
   const FG = '#fff';
   const BRAND = getComputedStyle(document.documentElement).getPropertyValue('--brand-color');
@@ -124,6 +123,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
   const C = document.getElementById('canvas');
   const verticesRange = document.getElementById('vertices');
   const volumeRange = document.getElementById('volume');
+  const trailRange = document.getElementById('trail');
   const playDemo1 = document.getElementById('demo1');
   const playDemo2 = document.getElementById('demo2');
   const playDemo3 = document.getElementById('demo3');
@@ -131,7 +131,8 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
   const playButton = document.getElementById('play');
   const $ = C.getContext('2d');
 
-  const FF = 64;
+  let vertices = ~~(FF * 2 / parseInt(verticesRange.value, 10));
+
   verticesRange.setAttribute('min', 3);
   verticesRange.setAttribute('max', FF * 2);
   verticesRange.setAttribute('value', FF);
@@ -139,18 +140,24 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
   volumeRange.setAttribute('max', 1);
   volumeRange.setAttribute('step', 0.1);
   volumeRange.setAttribute('value', VOLUME);
-  const STEP = W / FF;
-  let MOD = ~~(FF * 2 / parseInt(verticesRange.value, 10));
+  trailRange.setAttribute('min', 0.1);
+  trailRange.setAttribute('max', 1);
+  trailRange.setAttribute('step', 0.05);
+  trailRange.setAttribute('value', OPACITY);
 
   const audiolizer = new _index2.default({ fftSize: FF });
   audiolizer.volume = VOLUME;
 
   verticesRange.addEventListener('input', e => {
-    MOD = ~~(FF * 2 / parseInt(e.target.value, 10));
+    vertices = ~~(FF * 2 / parseInt(e.target.value, 10));
   });
 
   volumeRange.addEventListener('input', e => {
     audiolizer.volume = parseFloat(e.target.value);
+  });
+
+  trailRange.addEventListener('input', e => {
+    $.fillStyle = `hsla(0, 0%, 0%, ${parseFloat(e.target.value)})`;
   });
 
   playDemo1.addEventListener('click', e => {
@@ -188,9 +195,9 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
     const revArr = [...arr].reverse();
     const roundArr = [...arr, ...revArr];
     let start;
-    const filteredArray = roundArr.filter((x, i) => !Boolean(i % MOD));
+    const filteredArray = roundArr.filter((x, i) => !Boolean(i % vertices));
     filteredArray.forEach((point, i) => {
-      const rad = part / filteredArray.length * i;
+      const rad = 2 * Math.PI / filteredArray.length * i;
       const x = Math.sin(rad) * point + W / 2;
       const y = Math.cos(rad) * point + H / 2;
       if (!start) {
