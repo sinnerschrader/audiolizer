5f3c43e9c1a7c3c1bdc17810f8284aefcd9bfb9f
Gregor Adams <greg@pixelass.com>
2018-01-19 00:01:40 +0100

    docs(demo): added trail option
    clean up and more config

--- app/index.js
+++ app/index.js
@@ -6,11 +6,10 @@ import DEMO_AUDIO_3 from './audio/car.mp3'
 const W = 512
 const H = 512
 const LINE_WIDTH = 3
-const OPACITY = 1
 const VOLUME = 0.2
-
-const ROUND = 1
-const part = 2 * Math.PI
+const OPACITY = 0.2
+const FF = 64
+const STEP = W / FF
 const BG = '#000'
 const FG = '#fff'
 const BRAND = getComputedStyle(document.documentElement).getPropertyValue('--brand-color')
@@ -19,6 +18,7 @@ const BRAND = getComputedStyle(document.documentElement).getPropertyValue('--bra
 const C = document.getElementById('canvas')
 const verticesRange = document.getElementById('vertices')
 const volumeRange = document.getElementById('volume')
+const trailRange = document.getElementById('trail')
 const playDemo1 = document.getElementById('demo1')
 const playDemo2 = document.getElementById('demo2')
 const playDemo3 = document.getElementById('demo3')
@@ -26,7 +26,9 @@ const stopButton = document.getElementById('stop')
 const playButton = document.getElementById('play')
 const $ = C.getContext('2d')
 
-const FF = 64
+
+let vertices = ~~(FF * 2 / parseInt(verticesRange.value, 10))
+
 verticesRange.setAttribute('min', 3)
 verticesRange.setAttribute('max', FF * 2)
 verticesRange.setAttribute('value', FF)
@@ -34,8 +36,10 @@ volumeRange.setAttribute('min', 0)
 volumeRange.setAttribute('max', 1)
 volumeRange.setAttribute('step', 0.1)
 volumeRange.setAttribute('value', VOLUME)
-const STEP = W / FF
-let MOD = ~~(FF * 2 / parseInt(verticesRange.value, 10))
+trailRange.setAttribute('min', 0.1)
+trailRange.setAttribute('max', 1)
+trailRange.setAttribute('step', 0.05)
+trailRange.setAttribute('value', OPACITY)
 
 
 
@@ -44,11 +48,15 @@ audiolizer.volume = VOLUME
 
 
 verticesRange.addEventListener('input', e => {
-  MOD = ~~(FF * 2 / parseInt(e.target.value, 10))
+  vertices = ~~(FF * 2 / parseInt(e.target.value, 10))
 })
 
 volumeRange.addEventListener('input', e => {
   audiolizer.volume = parseFloat(e.target.value)
+})
+
+trailRange.addEventListener('input', e => {
+  $.fillStyle = `hsla(0, 0%, 0%, ${parseFloat(e.target.value)})`
 })
 
 playDemo1.addEventListener('click', e => {
@@ -86,9 +94,9 @@ audiolizer.convert = (arr = []) => {
   const revArr = [...arr].reverse()
   const roundArr = [...arr, ...revArr]
   let start
-  const filteredArray = roundArr.filter((x, i) => !Boolean(i % MOD))
+  const filteredArray = roundArr.filter((x, i) => !Boolean(i % vertices))
   filteredArray.forEach((point, i) => {
-    const rad = part / filteredArray.length * i
+    const rad = 2 * Math.PI / filteredArray.length * i
     const x = Math.sin(rad) * point + W / 2
     const y = Math.cos(rad) * point + H / 2
     if (!start) {
